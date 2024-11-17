6e2466388897a7e1c2e8bc51872bab07863f0cde
shongCoder <soyeong163@gmail.com>
2024-11-17 23:23:16 +0900

    PSY_NavibarShop: 스타일 수정 TabBar: 그림자 추가 pakage.json: 가로 스크롤 플로그인 추가 tailwind.config: 가로 스크롤 플러그인 추가 ProductReadPage: 로직 분리 ProductReadComponent: 전체적으로 수정함, 디자인, 수량증감 추가 ReviewByProductComponent: 디자인 작업

--- src/component/product/ProductReadComponent.jsx
+++ src/component/product/ProductReadComponent.jsx
@@ -7,21 +7,24 @@ import ReviewByProductComponent from "../review/ReviewByProductComponent.jsx";
 function ProductReadComponent() {
 
     const { pno } = useParams();
-    const [products, setProducts] = useState([]);
+    const [product, setProduct] = useState();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [showModal, setShowModal] = useState(false); // 모달 상태 추가
+    const [selectQnt, setSelectQnt] = useState(false);
+    const [qnt, setQnt] = useState(1);
     const navigate = useNavigate();
 
     useEffect(() => {
         const loadProduct = async () => {
             try {
-                const productData = await getProductOne(pno);
-                const productList = Array.isArray(productData) ? productData : [productData]; // 배열로 변환
-                setProducts(productList);
+                setLoading(true);
+                await getProductOne(pno).then(result => {
+                    setProduct(result);
+                    setLoading(false);
+                })
                 console.log("====================Product List ==================")
-                console.log(productList)
-                setLoading(false);
+                console.log(product);
             } catch (err) {
                 console.error('Error fetching product:', err);
                 setError('Failed to fetch product');
@@ -31,6 +34,7 @@ function ProductReadComponent() {
 
         loadProduct();
     }, [pno]);
+
 
     const handlePurchaseClick = (product) => {
         navigate("/order/create", { state: { products: [product] } });
@@ -58,60 +62,128 @@ function ProductReadComponent() {
     if (loading) return <div className="flex items-center justify-center min-h-screen">Loading...</div>;
     if (error) return <div className="text-red-500 text-center mt-4">{error}</div>;
 
+    // 수량 증감
+    const handleIncrease = () => {
+        setQnt(prevQnt => prevQnt + 1); // 수량 증가
+    };
+
+    const handleDecrease = () => {
+        setQnt(prevQnt => (prevQnt > 1 ? prevQnt - 1 : 1)); // 수량 감소 (최소값 1 유지)
+    };
+
+    const handleQntChange = (e) => {
+        const value = Math.max(1, parseInt(e.target.value) || 1); // 최소값 1 유지
+        setQnt(value);
+    };
+
 
     return (
-        <>
+        <div className="pb-40 text-bara_sodomy">
             <div>
-                {products.map((product) => (
-                    <div key={product.pno} className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <div>
-                            <CarouselComponent images={product.fileName || ['/images/star_1.svg']}/>
-                        </div>
-
-                        <div className="flex justify-between m-0">
-                            <h2 className="text-xl break-words">{product.pname}</h2>
-                            <img src='../../../public/images/star_5.svg' className='ml-2 object-contain h-full'
-                                 alt="Star rating"/>
-                        </div>
-
-                        <p className="text-2xl font-semibold">{product.price} 원</p>
-
-                        <hr/>
-                        <div>
-                            <h2 className="text-3xl font-medium p-2">상세 이미지</h2>
-                            <img src={product.fileName || "/images/M1.png"} className="w-full h-auto"
-                                 alt="Detail Image 1"/>
-                            <img src={product.fileName || "/images/M1.png"} className="w-full h-auto"
-                                 alt="Detail Image 2"/>
-                            <img src={product.fileName || "/images/M1.png"} className="w-full h-auto"
-                                 alt="Detail Image 3"/>
-                        </div>
+                <div key={product.pno} className="grid grid-cols-1 md:grid-cols-2 mb-4">
+                    <div className="w-full aspect-square bg-bara_gray_3">
+                            <CarouselComponent images={product.fileName}/>
                     </div>
-                ))}
+
+                    {/* 별점, 상품명, 가격 */}
+                    <div className="mb-4 bg-white px-8 py-8">
+                        <img src='/images/star_1.svg' className='ml-auto w-28'
+                             alt="Star rating"/>
+                        <h2 className="text-[1.2rem]">{product.pname}</h2>
+                        <hr className="my-4 border-bara_gray_3" />
+                        <p className="text-2xl font-semibold text-bara_blue">{product.price} 원</p>
+                    </div>
+
+                    {/* 상세 이미지 */}
+                    <div className="bg-white px-8 py-8 mb-4">
+                        <h3 className="text-[1.2rem] font-bold">상품 정보</h3>
+                        <hr className="my-4 border-bara_gray_3"/>
+                        <img src={product.fileName || "/images/M1.png"} className="w-full h-auto"
+                             alt="Detail Image 1"/>
+                        <img src={product.fileName || "/images/M1.png"} className="w-full h-auto"
+                             alt="Detail Image 2"/>
+                        <img src={product.fileName || "/images/M1.png"} className="w-full h-auto"
+                             alt="Detail Image 3"/>
+                    </div>
+
+                    {/* 리뷰 */}
+                    <div className="bg-white px-8 py-8">
+                        <h3 className="text-[1.2rem] font-bold">리뷰</h3>
+                        <hr className="my-4 border-bara_gray_3"/>
+                        <ReviewByProductComponent pno={pno}/>
+                    </div>
+
+                </div>
             </div>
 
-            <ReviewByProductComponent pno={pno} />
-
-            <div className="text-center mt-6">
+            <div
+                className="
+                    fixed bottom-0 left-0 w-full h-28 bg-white text-bara_sodomy flex justify-around items-start px-8
+                "
+                style={{ boxShadow: '0 -2px 10px rgba(0, 0, 0, 0.1)' }}
+            >
                 <button
-                    onClick={() => handlePurchaseClick(products[0])} // 첫 번째 제품 선택
-                    className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-5 rounded-lg shadow-md transition-colors"
+                    onClick={() => setSelectQnt(true)}
+                    className="w-full py-4 bg-bara_blue text-white rounded-[0.5rem] mt-4"
                 >
                     구매하기
                 </button>
-                <button
-                    onClick={() => handleAddCartClick(products[0].pno)} // 첫 번째 제품 선택
-                    className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-5 rounded-lg shadow-md transition-colors ml-4"
-                >
-                    장바구니에 담기
-                </button>
             </div>
+
+
+            {/*수량 체크 및 장바구니 추가, 구매하기*/}
+            {selectQnt &&
+                <div
+                    className="fixed bottom-0 left-0 w-full bg-white text-bara_sodomy px-8"
+                    style={{ boxShadow: '0 -2px 10px rgba(0, 0, 0, 0.1)' }}
+                >
+                    <img src="/images/close.svg" className="ml-auto w-4 mt-8" onClick={() => setSelectQnt(false)} />
+                    <div>
+                        <div className="bg-bara_gray_1 flex p-4 mt-4 justify-between">
+                            <span>수량</span>
+                            <div>
+                                <button onClick={handleDecrease} className="px-2 bg-bara_gray_2">-</button>
+                                <input
+                                    onChange={handleQntChange}
+                                    value={qnt}
+                                    type="number"
+                                    className="w-8 text-center"
+                                />
+                                <button onClick={handleIncrease} className="px-2 bg-bara_gray_2">+</button>
+                            </div>
+                        </div>
+                        <hr className="border-bara_gray_2 my-4" />
+                        <div className="flex justify-between mb-4 font-bold">
+                            <span>총 {qnt}개</span>
+                            <span>{product.price * qnt}원</span>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div className="flex gap-4 mb-12">
+                        <button
+                            onClick={() => handleAddCartClick(product.pno)}
+                            className="w-full bg-bara_sky_blue py-4 text-white rounded-[0.5rem]"
+                        >
+                            장바구니에 담기
+                        </button>
+                        <button
+                            onClick={() => handlePurchaseClick(product)}
+                            className="w-full bg-bara_blue py-4 text-white rounded-[0.5rem]"
+                        >
+                            구매하기
+                        </button>
+                    </div>
+
+                </div>
+            }
 
             {/* 모달 */}
             {showModal && (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
                     <div className="bg-white p-6 rounded-lg shadow-xl w-11/12 max-w-md">
-                        <h2 className="text-xl font-bold text-center">장바구니에 담았습니다</h2>
+                    <h2 className="text-xl font-bold text-center">장바구니에 담았습니다</h2>
                         <div className="flex justify-center mt-6 space-x-4">
                             <button
                                 onClick={() => handleMoveCartClick()}
@@ -129,7 +201,7 @@ function ProductReadComponent() {
                     </div>
                 </div>
             )}
-        </>
+        </div>
     );
 }
 
