93edc3559b5f3751cfd5b370f21a32ac9e93fb5b
shongCoder <soyeong163@gmail.com>
2024-11-27 18:43:54 +0900

    PSY_App,main: fcm 설정 customerAPI: fcm 설정 firebase~: fcm 설정 vite.config.js: fcm 때문에 설정 바꿈 productAPI: 안쓰는 임포트 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import './App.css'
-
+import { getToken, onMessage } from "firebase/messaging";
+import { messaging } from "./firebase/firebaseConfige.js";
+import {useEffect} from "react";
 
 function App() {
+
+    async function requestPermission() {
+        //requesting permission using Notification API
+        const permission = await Notification.requestPermission();
+
+        if (permission === "granted") {
+            const token = await getToken(messaging, {
+                vapidKey: 'BD0vjI3VPUoiRygaJB4DT4t8p8y88_1haNnpODwZxwPSn4MZ5j459yMScSvCjSrRlqRC4mmLqyM6YYkeCXOKrkI',
+            });
+
+            //We can send token to server
+            console.log("Token generated : ", token);
+
+        } else if (permission === "denied") {
+            //notifications are blocked
+            alert("You denied for the notification");
+        }
+    }
+
+    useEffect(() => {
+        requestPermission();
+    }, []);
+
+    onMessage(messaging, (payload) => {
+        console.log(payload);
+        alert("On Message ")
+    });
 
 
     return (
