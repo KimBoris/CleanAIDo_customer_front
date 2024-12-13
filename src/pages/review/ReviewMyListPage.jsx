7602af5c3558b87b031570cc825524fc46bbf6ba
shongCoder <soyeong163@gmail.com>
2024-12-13 17:38:43 +0900

    PSY_Review~: 사용자별 리뷰, 리뷰삭제 구현 ModalComponent: 모달 컴포넌트 OrderLisrPage: 뒤로가기 버튼 경로 추가

--- src/pages/review/ReviewMyListPage.jsx
+++ src/pages/review/ReviewMyListPage.jsx
@@ -1,7 +1,14 @@
+import NaviBarTitle from "../../component/layout/NaviBarTitle.jsx";
+import TabBarShop from "../../component/layout/TabBarShop.jsx";
+import ReviewMyListComponent from "../../component/review/ReviewMyListComponent.jsx";
 
 function ReviewMyListPage() {
     return (
-        <div></div>
+        <div>
+            <NaviBarTitle title={"리뷰관리"} path={"/mypage"} />
+            <ReviewMyListComponent />
+            <TabBarShop />
+        </div>
     );
 }
 
