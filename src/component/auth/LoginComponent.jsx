adb3fe4b97b77dffc19787a9de6624dbddbf7a5f
Taehwan Kim <kyb062855@naver.com>
2024-12-16 19:40:00 +0900

    KTH_로그인 화면 디자인 수정 주문 장바구니 수정 병합완료

--- src/component/auth/LoginComponent.jsx
+++ src/component/auth/LoginComponent.jsx
@@ -5,7 +5,7 @@ const LoginComponent = () => {
 
     const handleKakaoLogin = () => {
         const clientId = "bd7725f821010811fbfbb131b8f9985d"; // REST API 키
-        const redirectUri = "http://10.10.10.151:5173/oauth/kakao/callback"; // Redirect URI
+        const redirectUri = "http://localhost:5173/oauth/kakao/callback"; // Redirect URI
         const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
         window.location.href = kakaoAuthUrl; // 카카오 로그인 페이지로 이동
     };
