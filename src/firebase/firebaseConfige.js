2c613227c6869f3fcb618861f61a2e9623ca94c7
shongCoder <soyeong163@gmail.com>
2024-12-21 13:42:33 +0900

    PSY_firebase설정

--- src/firebase/firebaseConfige.js
+++ src/firebase/firebaseConfige.js
@@ -1,22 +1,22 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getMessaging } from "firebase/messaging";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-//
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: import.meta.env.VITE_FCM_API_KEY,
-//     authDomain: "cleanaido-29788.firebaseapp.com",
-//     projectId: "cleanaido-29788",
-//     storageBucket: "cleanaido-29788.firebasestorage.app",
-//     messagingSenderId: "700069476960",
-//     appId: "1:700069476960:web:5219966c017dc50790e105",
-//     measurementId: "G-X32L7ZE4XR"
-// };
-//
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-//
-// export const messaging = getMessaging(app);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FCM_API_KEY,
+    authDomain: "cleanaido-29788.firebaseapp.com",
+    projectId: "cleanaido-29788",
+    storageBucket: "cleanaido-29788.firebasestorage.app",
+    messagingSenderId: "700069476960",
+    appId: "1:700069476960:web:5219966c017dc50790e105",
+    measurementId: "G-X32L7ZE4XR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const messaging = getMessaging(app);
